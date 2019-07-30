ea8a406b9495fda1609f7c6f92d1d64a0630c749
momijiame <1781355+momijiame@users.noreply.github.com>
2019-07-30 15:26:04 +0900

    initial commit

---
+++ japanize_kivy/japanizer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+import pathlib
+
+from kivy.resources import resource_add_path
+from kivy.core.text import LabelBase
+from kivy.core.text import DEFAULT_FONT
+
+
+RESOURCE_PATH = pathlib.Path(__file__).parent / 'resources/ipaexg00401'
+
+
+def japanize():
+    resource_add_path(RESOURCE_PATH)
+    LabelBase.register(DEFAULT_FONT, 'ipaexg.ttf')
+
+
+def show_license():
+    license_agreement_filepath = RESOURCE_PATH / 'IPA_Font_License_Agreement_v1.0.txt'
+    with open(license_agreement_filepath, mode='r') as fp:
+        print(fp.read(), file=sys.stderr)
